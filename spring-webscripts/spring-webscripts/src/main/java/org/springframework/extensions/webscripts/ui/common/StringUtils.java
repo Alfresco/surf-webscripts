452e1dfd912a61463fd64e9808352213140b1dd6
Alan Davis <alan.davis@alfresco.com>
2015-08-08 08:04:15 +0000

    Merged 5.0.N (5.0.3) to trunk (5.1)    109725: Merged 5.0.2 (5.0.2) to 5.0.N (5.0.3)       109667: Fix for MNT-14508

--- spring-webscripts/spring-webscripts/src/main/java/org/springframework/extensions/webscripts/ui/common/StringUtils.java
+++ spring-webscripts/spring-webscripts/src/main/java/org/springframework/extensions/webscripts/ui/common/StringUtils.java
@@ -390,7 +390,7 @@ public class StringUtils
         // perform a multi-pass strip until the length of the result stays fixed
         try
         {
-            final int MaxPasses = 3;  // Maximum number of passes over the content
+            final int MaxPasses = 2;  // Maximum number of passes over the content
             for (int i=0; i<MaxPasses; i++)
             {
                 strippedLength = result.length();
@@ -405,11 +405,11 @@ public class StringUtils
                 
                 result = buf.toString();
                 
-                if (strippedLength == result.length()) break;
+                if (strippedLength >= result.length()) break;
             }
             // avoid attempts to add attack vectors that wrap in partial attacks hoping that the stripping
             // process will effectively reconstruct attack elements as other elements are removed.
-            if (strippedLength != result.length())
+            if (strippedLength < result.length())
             {
                 return "";
             }
@@ -512,111 +512,91 @@ public class StringUtils
                     Vector<Attribute> attrs = tag.getAttributesEx();
                     
                     // tag attributes contain the tag name at a minimum
-                    if (attrs.size() > 1)
+                    buf.append('<').append(tag.getRawTagName());
+                    for (Attribute attr : attrs)
                     {
-                        buf.append('<').append(tag.getRawTagName());
-                        for (Attribute attr : attrs)
+                        String name = attr.getName();
+                        if (name != null)
                         {
-                            String name = attr.getName();
-                            if (name != null)
+                            String nameUpper = name.toUpperCase();
+                            if (!tagname.equals(nameUpper))   // ignore tag name itself
                             {
-                                String nameUpper = name.toUpperCase();
-                                if (!tagname.equals(nameUpper))   // ignore tag name itself
-                                {
-                                    // strip any non-alpha character from attribute name - can be used to form XSS attacks
-                                    // i.e. allow onclick= by using /onclick= or "onclick=
-                                    String safeName = nameUpper.replaceAll("[^A-Z_]", "");
+                                // strip any non-alpha character from attribute name - can be used to form XSS attacks
+                                // i.e. allow onclick= by using /onclick= or "onclick=
+                                String safeName = nameUpper.replaceAll("[^A-Z_]", "");
 
-                                    // found a tag attribute for output
-                                    // test for known attributes to remove
-                                    if (!safeName.startsWith(ATTR_ON_PREFIX) && !attrBlackList.contains(safeName))
+                                // found a tag attribute for output
+                                // test for known attributes to remove
+                                if (!safeName.startsWith(ATTR_ON_PREFIX) && !attrBlackList.contains(safeName))
+                                {
+                                    String value = attr.getRawValue();
+                                    
+                                    //Check for forbidden values on particular attribute
+                                    if (attrValueBlackList.containsKey(safeName))
                                     {
-                                        String value = attr.getRawValue();
-                                        
-                                        //Check for forbidden values on particular attribute
-                                        if (attrValueBlackList.containsKey(safeName))
+                                        if (attr.getValue() != null)
                                         {
-                                            if (attr.getValue() != null)
+                                            String test = attr.getValue().trim().toUpperCase();
+                                            Set<String> blackValues = attrValueBlackList.get(safeName);
+                                            if (blackValues.contains(test))
                                             {
-                                                String test = attr.getValue().trim().toUpperCase();
-                                                Set<String> blackValues = attrValueBlackList.get(safeName);
-                                                if (blackValues.contains(test))
+                                                value = "\"\"";
+                                            }
+                                        }
+                                    }
+                                    
+                                    // sanitise src and href attributes
+                                    if (attrGreyList.contains(safeName))
+                                    {
+                                        // test the attribute value for XSS - the procedure is:
+                                        // . first trim the string to remove whitespace at front (hides attack)
+                                        // . test for encoded characters at start of attribute (hides javascript)
+                                        // . test for direct javascript: attack
+                                        if (attr.getValue() != null)
+                                        {
+                                            String test = attr.getValue().trim();
+                                            if (test.length() > 2)
+                                            {
+                                                // handle that html encoder doesn't know about grave accent
+                                                if (test.startsWith("`"))
+                                                {
+                                                    test = test.substring(1);
+                                                }
+                                                // all encoded attacks start with &# sequence - assume to be an attack
+                                                // there are no valid protocols starting with "J" - assume attack
+                                                // the "background" attribute is also vulnerable - no web colors start with "J"
+                                                // on IE6 "vbscript" can be used - no colors or protocols start with "VB"
+                                                if (test.startsWith("&#") ||
+                                                    test.substring(0, 1).toUpperCase().charAt(0) == 'J' ||
+                                                    test.substring(0, 2).toUpperCase().startsWith("VB"))
                                                 {
                                                     value = "\"\"";
                                                 }
                                             }
                                         }
-                                        
-                                        // sanitise src and href attributes
-                                        if (attrGreyList.contains(safeName))
-                                        {
-                                            // test the attribute value for XSS - the procedure is:
-                                            // . first trim the string to remove whitespace at front (hides attack)
-                                            // . test for encoded characters at start of attribute (hides javascript)
-                                            // . test for direct javascript: attack
-                                            if (attr.getValue() != null)
-                                            {
-                                                String test = attr.getValue().trim();
-                                                if (test.length() > 2)
-                                                {
-                                                    // handle that html encoder doesn't know about grave accent
-                                                    if (test.startsWith("`"))
-                                                    {
-                                                        test = test.substring(1);
-                                                    }
-                                                    // all encoded attacks start with &# sequence - assume to be an attack
-                                                    // there are no valid protocols starting with "J" - assume attack
-                                                    // the "background" attribute is also vulnerable - no web colors start with "J"
-                                                    // on IE6 "vbscript" can be used - no colors or protocols start with "VB"
-                                                    if (test.startsWith("&#") ||
-                                                        test.substring(0, 1).toUpperCase().charAt(0) == 'J' ||
-                                                        test.substring(0, 2).toUpperCase().startsWith("VB"))
-                                                    {
-                                                        value = "\"\"";
-                                                    }
-                                                }
-                                            }
-                                        }
-                                        buf.append(' ').append(name);
-                                        if (value != null)
-                                        {
-                                            buf.append('=').append(value);
-                                        }
+                                    }
+                                    buf.append(' ').append(name);
+                                    if (value != null)
+                                    {
+                                        buf.append('=').append(value);
                                     }
                                 }
                             }
                         }
-
-                        // close the tag after attribute output and before child output
-                        buf.append('>');
-                        
-                        // process children if they exist
-                        if (tag.getChildren() != null)
-                        {
-                            processNodes(buf, tag.getChildren().elements(), encode, false);
-                        }
-                        // add the end-tag only if the tag is not a self-closing one <tag attr="val"/>
-                        if (tag.getEndTag() != null && tag.getEndTag() != tag)
-                        {
-                            buf.append(tag.getEndTag().toHtml());
-                        }
                     }
-                    else
+
+                    // close the tag after attribute output and before child output
+                    buf.append('>');
+                    
+                    // process children if they exist
+                    if (tag.getChildren() != null)
                     {
-                        // process children if they exist
-                        if (tag.getChildren() != null)
-                        {
-                            buf.append('<').append(tag.getRawTagName()).append('>');
-                            processNodes(buf, tag.getChildren().elements(), encode, false);
-                            if (tag.getEndTag() != null)
-                            {
-                                buf.append(tag.getEndTag().toHtml());
-                            }
-                        }
-                        else
-                        {
-                            buf.append(tag.toHtml());
-                        }
+                        processNodes(buf, tag.getChildren().elements(), encode, false);
+                    }
+                    // add the end-tag only if the tag is not a self-closing one <tag attr="val"/>
+                    if (tag.getEndTag() != null && tag.getEndTag() != tag)
+                    {
+                        buf.append(tag.getEndTag().toHtml());
                     }
                 }
             }
