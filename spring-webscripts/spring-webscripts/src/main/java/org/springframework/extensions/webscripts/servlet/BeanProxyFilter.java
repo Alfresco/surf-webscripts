ea4854fac3ba5bacfb05d9c0e0aa3bcd599ab2a5
Kevin Roast <kevin.roast@alfresco.com>
2016-10-05 13:56:55 +0000

    SHA-1572 - Share SSO filter classname SSOAuthenticationFilter hard-coded in Share web.xml. Moved BeanProxyFilter to webscripts.

---
+++ spring-webscripts/spring-webscripts/src/main/java/org/springframework/extensions/webscripts/servlet/BeanProxyFilter.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (C) 2005-2009 Alfresco Software Limited.
+ *
+ * This file is part of the Spring Surf Extension project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.extensions.webscripts.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+/**
+ * An adapter from the servlet filter world into the Spring dependency injected world. Simply looks up a
+ * {@link DependencyInjectedFilter} with a configured bean name and delegates the
+ * {@link #doFilter(ServletRequest, ServletResponse, FilterChain)} call to that. This allows us to swap in and out
+ * different implementations for different 'hook points' in web.xml.
+ * 
+ * @author dward
+ */
+public class BeanProxyFilter implements Filter
+{
+    /**
+     * Name of the init parameter that carries the proxied bean name 
+     */
+    private static final String INIT_PARAM_BEAN_NAME = "beanName";
+    
+    private DependencyInjectedFilter filter;
+    private ServletContext context;    
+    
+    /**
+     * Initialize the filter.
+     * 
+     * @param args  FilterConfig
+     * @throws ServletException the servlet exception
+     * @exception ServletException
+     */
+    public void init(FilterConfig args) throws ServletException
+    {
+        WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(args.getServletContext());
+        this.filter = (DependencyInjectedFilter)ctx.getBean(args.getInitParameter(INIT_PARAM_BEAN_NAME));
+        this.context = args.getServletContext();
+    }
+
+    /* (non-Javadoc)
+     * @see javax.servlet.Filter#destroy()
+     */
+    public void destroy()
+    {
+        this.filter = null;
+    }
+
+    /* (non-Javadoc)
+     * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException
+    {
+        this.filter.doFilter(this.context, request, response, chain);
+    }
+}
---
+++ spring-webscripts/spring-webscripts/src/main/java/org/springframework/extensions/webscripts/servlet/DependencyInjectedFilter.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (C) 2005-2009 Alfresco Software Limited.
+ *
+ * This file is part of the Spring Surf Extension project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.extensions.webscripts.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * A bean-like equivalent of a servlet filter, designed to be managed by a Spring container.
+ * 
+ * @see BeanProxyFilter
+ * @author dward
+ */
+public interface DependencyInjectedFilter
+{
+    /**
+     * The <code>doFilter</code> method of the Filter is called by the container each time a request/response pair is
+     * passed through the chain due to a client request for a resource at the end of the chain. The FilterChain passed
+     * in to this method allows the Filter to pass on the request and response to the next entity in the chain.
+     * <p>
+     * A typical implementation of this method would follow the following pattern:- <br>
+     * 1. Examine the request<br>
+     * 2. Optionally wrap the request object with a custom implementation to filter content or headers for input
+     * filtering <br>
+     * 3. Optionally wrap the response object with a custom implementation to filter content or headers for output
+     * filtering <br>
+     * 4. a) <strong>Either</strong> invoke the next entity in the chain using the FilterChain object (
+     * <code>chain.doFilter()</code>), <br>
+     * 4. b) <strong>or</strong> not pass on the request/response pair to the next entity in the filter chain to block
+     * the request processing<br>
+     * 5. Directly set headers on the response after invocation of the next entity in the filter chain.
+     **/
+    public void doFilter(ServletContext context, ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException;
+}
