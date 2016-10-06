31400b62bce009ae2c2ed7519978334541447458
Kevin Roast <kevin.roast@alfresco.com>
2016-10-06 11:03:31 +0000

    Fix packages after move

--- spring-webscripts/spring-webscripts/src/main/java/org/springframework/extensions/webscripts/servlet/CSRFFilter.java
+++ spring-webscripts/spring-webscripts/src/main/java/org/springframework/extensions/webscripts/servlet/CSRFFilter.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
  */
-package org.springframework.extensions.surf.site.servlet;
+package org.springframework.extensions.webscripts.servlet;
 
 import java.io.IOException;
 import java.security.SecureRandom;
