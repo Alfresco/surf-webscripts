67abb9af97248ca8c469e18066fcbf6125c3dd3b
kshah <kavit.shah@globallogic.com>
2023-01-16 12:04:27 +0530

    Added changes for release process

---
+++ _ci/release.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+# Use full history for release
+git checkout -B "$BRANCH_NAME"
+
+#sync the branch by getting the latest changes
+git pull
+
+# Add email to link commits to user
+git config user.email "${GIT_EMAIL}"
+
+mvn install -DskipTests=true -B -V
+
+mvn --batch-mode -q \
+-DskipTests \
+-Dusername="${GIT_USERNAME}" \
+-Dpassword="${GIT_PASSWORD}" \
+-DscmCommentPrefix="[maven-release-plugin][skip ci]" \
+"-Darguments=-DskipTests " \
+release:clean release:prepare release:perform
+
